c0bde3f19d6bed0701dbbfb5af5f7b5c669436a8
phidiascheng <59988253+phidiascheng@users.noreply.github.com>
2020-09-23 00:54:01 +0800

    Delete BingWallPaper.sh

--- BingWallPaper.sh
+++
@@ -1,3 +0,0 @@
-curl -o ~/Library/Caches/bing.zip https://codeload.github.com/phidiascheng/bingWallPaper/zip/master 
-unzip ~/Library/Caches/bing.zip -d ~/Library/Caches
-python3 ~/Library/Caches/bingWallPaper-master/bing.py
