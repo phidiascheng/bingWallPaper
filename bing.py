c5b748adf44d9e4b3a3b77341ea83095251177b1
phidiascheng <59988253+phidiascheng@users.noreply.github.com>
2020-09-23 02:26:43 +0800

    Update bing.py

--- bing.py
+++ bing.py
@@ -1,25 +1,30 @@
 import urllib.request
 import requests         
-import os
+import os,datetime
 from bs4 import BeautifulSoup
 
-raw_img_url = "https://cn.bing.com/HPImageArchive.aspx?n=1"
-soup = BeautifulSoup(requests.get(raw_img_url).text,"html.parser")
-img_url = 'https://cn.bing.com'+soup.find('url').text
 dirname=os.environ['HOME']+"/Pictures/Bing/"
 if not os.path.exists(dirname):
     os.mkdir(dirname)
-try:
-    urllib.request.urlretrieve(img_url,dirname+'temp')
-except IOError as e:
-    pass
-except Exception as e:
-    pass
-for i in range(30):
-    filename = str(30-i)+'.jpg'
-    if os.path.exists(dirname+filename):
-        if filename == '30.jpg':
-            os.remove(dirname+filename)
-        else:
-            os.rename(dirname+filename,dirname+str(31-i)+'.jpg')
-os.rename(dirname+'temp',dirname+'1.jpg')
+
+date = datetime.datetime.now()
+month = date.strftime('%m')
+day = date.strftime('%d')
+filename = month+'_'+day+'.jpg'
+
+if not os.path.exists(dirname+filename):
+    prevMonth = 12 if month == 1 else (int(month) - 1)%12
+    prevMonStr = str(prevMonth) if prevMonth >= 10 else '0'+str(prevMonth)
+    prevFileName = prevMonStr+'_'+day+'.jpg'
+    if os.path.exists(dirname+prevFileName):
+        os.remove(dirname+prevFileName)
+
+    raw_img_url = "https://cn.bing.com/HPImageArchive.aspx?n=1"
+    soup = BeautifulSoup(requests.get(raw_img_url).text,"html.parser")
+    img_url = 'https://cn.bing.com'+soup.find('url').text
+    try:
+        urllib.request.urlretrieve(img_url,dirname+filename)
+    except IOError as e:
+        pass
+    except Exception as e:
+        pass
